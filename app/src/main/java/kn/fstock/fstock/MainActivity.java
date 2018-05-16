df2686612369e939ed9d7a54e3390b7be16a77e7
André Luiz <maki.karasu@gmail.com>
2018-05-16 18:14:18 -0300

    foi2

--- app/src/main/java/kn/fstock/fstock/MainActivity.java
+++ app/src/main/java/kn/fstock/fstock/MainActivity.java
@@ -1,13 +1,21 @@
 package kn.fstock.fstock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void singIn(View v){
+        startActivity(new Intent(getBaseContext(), SingInActivity.class));
     }
 }
